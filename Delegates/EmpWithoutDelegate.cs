b8a3b1a4deb3104be496cc96b798aef2149400bb
ashishmodi <gashishmukesh92@gmail.com>
2017-01-14 20:59:09 +0530

    Delegate demo - EmpWithoutDelegate.cs

---
+++ Delegates/EmpWithoutDelegate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WithoutUsingDelegates
+{
+    class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public int Experince { get; set; }
+
+        public static void promoteEmpl(List<Employee> empList)
+        {
+            foreach(Employee emp in empList)
+            {
+                if (emp.Experince >= 5)
+                    Console.WriteLine(emp.Name + " is promoted");
+            }
+        }
+    }
+
+    class TestEmpWithoutDelegate
+    {
+        public static void testEmpWithoutDelegate()
+        {
+            List<Employee> empList = new List<Employee>();
+            empList.Add(new Employee { ID = 1, Name = "Abc", Experince = 3, Salary = 70000 });
+            empList.Add(new Employee { ID = 2, Name = "Def", Experince = 4, Salary = 80000 });
+            empList.Add(new Employee { ID = 3, Name = "Ghi", Experince = 5, Salary = 90000 });
+            empList.Add(new Employee { ID = 4, Name = "Jkl", Experince = 6, Salary = 100000 });
+
+            Employee.promoteEmpl(empList);
+        }
+    }
+}
