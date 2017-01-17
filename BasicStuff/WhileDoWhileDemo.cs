f9a8bf84ef329982daea8cee0f1e8a157a7180f2
ashishmodi <gashishmukesh92@gmail.com>
2017-01-17 19:54:01 +0530

    do-while loop demo

---
+++ BasicStuff/WhileDoWhileDemo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace BasicStuff
+{
+    class WhileDoWhileDemo
+    {
+        /*Input : Enter your target --> 10
+        Output : 0 2 4 6 8 10 
+        Do you still want to continue Yes or No */
+        public void processDoWhile()
+        {
+            string sChoice = "";
+            do
+            {
+                Console.WriteLine("Enter your target");
+                int iTarget = int.Parse(Console.ReadLine());
+
+                int iStart = 0;
+                while (iStart <= iTarget)
+                {
+                    Console.WriteLine(iStart + " ");
+                    iStart = iStart + 2;
+                }
+
+                do
+                {
+                    Console.WriteLine("\nDo you still want to continue - Yes or No ");
+                    sChoice = Console.ReadLine();
+                } while (sChoice != "Yes" && sChoice != "No");
+
+            } while (sChoice != "No");
+        }
+    }
+}
