288fe72526cb40c2a522017a179eee93a58c03c4
ashishmodi <gashishmukesh92@gmail.com>
2017-01-17 19:48:25 +0530

    Properties demo

---
+++ BasicStuff/Properties.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BasicStuff
+{
+    class Customer
+    {
+        public int Id { get; set; } //compiler internally creates memory for storage
+        public string Name { get; set; }
+        public void printCustomer()
+        {
+            Console.WriteLine("Name = {0}, Id = {1}", Name, Id);
+        }
+    }
+
+    class PropertiesDemo
+    {
+        public void testProperties()
+        {
+            Customer c1 = new Customer
+            {
+                Id = 10,
+                Name = "Ashish"
+            };
+            c1.printCustomer();
+        }
+    }
+}
