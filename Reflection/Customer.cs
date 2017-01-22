062c7c709cb404214ad11617d7c178397bbaa21a
ashishmodi <gashishmukesh92@gmail.com>
2017-01-22 21:08:00 +0530

    Customer class

---
+++ Reflection/Customer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Reflection
+{
+    class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Customer()
+        {
+            this.Id = -1;
+            this.Name = string.Empty;
+        }
+
+        public Customer(int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+
+        public void printId()
+        {
+            Console.WriteLine("Id = {0}", Id);
+        }
+
+        public void printName()
+        {
+            Console.WriteLine("Name = {0}", Name);
+        }
+
+        public void set(int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+    }
+}
