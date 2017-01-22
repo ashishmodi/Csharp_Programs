3e0cbb5924fefbae1cad963d8d2c6da6ed1a44a8
ashishmodi <gashishmukesh92@gmail.com>
2017-01-22 21:09:33 +0530

    Late binding using Reflaction

---
+++ Reflection/LateBinding.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reflection;
+
+namespace Reflection
+{
+    class LateBindingDemo
+    {
+        public static void testLateBinding()
+        {
+            try
+            {
+                Assembly executingAssembly = Assembly.GetExecutingAssembly();
+                Type type = executingAssembly.GetType("Reflection.Person");
+                MethodInfo method = type.GetMethod("getFullName");
+
+                string[] parameters = new string[2];
+                parameters[0] = "Ashish";
+                parameters[1] = "Modi";
+
+                object instance = Activator.CreateInstance(type);
+                string fullName = (string)method.Invoke(instance, parameters);
+                Console.WriteLine("Full name is " + fullName);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }        
+    }
+
+    class Person
+    {
+        public string getFullName(string first, string last)
+        {
+            return first + " " + last;
+        }
+    }
+}
